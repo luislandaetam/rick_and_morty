e37b4b73b6b87c7560979523d44a788c2f0d8f80
Luis Landaeta <luis.landaeta24@gmail.com>
2023-02-09 08:56:16 -0400

    Closing Button on Cards fixed

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,11 +4,13 @@ import styles from "./Cards.module.css";
 
 export default function Cards(props) {
   const { characters } = props;
+  const onClose = () => window.alert("Emulamos que se cierra la card");
   return (
     <div className={styles.characters}>
-      {characters.map((character) => (
-        <div>{Card(character)}</div>
-      ))}
+      {characters.map((character) => {
+        character.onClose = onClose;
+        return Card(character);
+      })}
     </div>
   );
 }
